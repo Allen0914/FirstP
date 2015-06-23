178f317de21b9c3f2088dff93ecf2b971e11f101
Allen0914 <liy@tradeshift.com>
2015-06-23 13:04:41 +0800

    hw2

--- src/p01/HW.java
+++ src/p01/HW.java
@@ -1,9 +1,11 @@
 package p01;
 
 public class HW {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		System.out.println("hello world!");
 		System.out.println("hello world!");
 
 	}
